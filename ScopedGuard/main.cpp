9154d338f873a52330155e3dea3a2d5230f823d4
Vladimir Ivanov <vladimir.d.ivanov@jetbrains.com>
2019-12-12 19:46:45 +0300

    ScopedGuard at C++17

--- ScopedGuard/main.cpp
+++ ScopedGuard/main.cpp
@@ -1,7 +1,7 @@
 #include <exception>
 #include "trace.h"
 
-
+//! C++11
 template <class Lambda> class AtScopeExit {
   Lambda& m_lambda;
 public:
@@ -22,20 +22,47 @@ public:
 
 #define Auto(...) Auto_INTERNAL2(__COUNTER__, __VA_ARGS__)
 
+//! C++17 ???
 
+template <class Exit, class Enter = Exit > class Scoped {
+	const Exit atExit_;
+	Scoped(const Scoped&) = delete;
+	Scoped(Scoped&&) = delete;
+	Scoped& operator=(const Scoped& other) = delete;
+	Scoped& operator=(Scoped&&) = delete;
+public:
+	explicit Scoped(const Exit& teardown) : atExit_(teardown) {}
+	Scoped(const Enter& init, const Exit& teardown) : Scoped(teardown) { init(); }
+	~Scoped() { atExit_(); }
+};
+
+template <class F2> Scoped(F2) -> Scoped<F2, F2>;
+template <class F1, class F2> Scoped(F1, F2) -> Scoped<F2, F1>;
+
+/// ^^^^^^^^^^^^^^^^^^^^^
 
 using namespace std;
 
-bool lock(int& x) { return (x = 1); }
-void unlock(int& x) { x = 0; }
+bool lock(int& x) { TraceF; return (++x); }
+void unlock(int& x) { TraceF; --x; }
+void unlock0() { TraceF; }
 
 void foo() {
+	TraceF;
 	throw runtime_error("Test exception");
 }
 
 void test(int& x) {
-	Auto(unlock(x));
-	lock(x);
+//	Auto(unlock(x)); // ok
+//	lock(x);
+
+	Scoped guard {
+		[&]() { lock(x); },
+		[&]() { unlock(x); }
+	};
+
+	Scoped atExit { []() { log_trace << "tear down!"; }};
+
 	TraceX(x);
 	foo();
 	unlock(x);
@@ -49,6 +76,7 @@ int main() {
 
 	try {
 		test(x);
+		log_warn << "This should be unavailable code!";
 	} catch (exception& e) {
 		log_info << e.what();
 	}
