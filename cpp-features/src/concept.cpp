fedd88f5718153fa5f95517c927cdd7d0e7c4ae6
Vladimir Ivanov <knebekaizer@gmail.com>
2022-10-26 21:55:05 +0400

    concept: Printable

--- cpp-features/src/concept.cpp
+++ cpp-features/src/concept.cpp
@@ -1,8 +1,77 @@
 //! \file
 
+#include <iostream>
+#include <type_traits>
 #include <concepts>
 
-struct ThreadPool {
-  void enqueue( std::Invocable auto fun );
+#include "trace.hpp"
+
+using namespace std;
+//struct ThreadPool {
+//  void enqueue( std::invocable auto fun );
+//};
+
+template <typename T>
+concept Printable = requires(T t, std::ostream& os) {
+    { t.print(os) } -> std::convertible_to<std::ostream&>; //std::ostream&;
 };
 
+struct P {
+    ostream& print(ostream& os) const { return os << "I am printable!"; };
+};
+
+template <Printable P>
+std::ostream& operator<<(std::ostream& os, P const& p) {
+    return p.print(os);
+}
+
+template <typename T>
+concept HasFoo = requires(T t, int i) {
+    { t.foo(i) } -> std::convertible_to<int>;
+};
+
+struct Foo {
+    int foo() const {  return 5; }
+};
+
+std::ostream& operator<<(std::ostream& os, Foo const& p) {
+    return os << "Foo: " << p.foo();
+}
+
+struct FooD : Foo {
+};
+
+struct Bar
+{
+    int foo(unsigned) {
+        return 5;
+    }
+};
+
+int main()
+{
+    cout << "Foo: ";
+    if constexpr (HasFoo<Foo>)
+        cout << "yes";
+    else
+        cout << "no";
+    cout << endl;
+
+    cout << "Bar: ";
+    if constexpr (HasFoo<Bar>)
+        cout << "yes";
+    else
+        cout << "no";
+    cout << endl;
+
+    cout << "P is Printable: ";
+    if constexpr (Printable<P>)
+        cout << "yes";
+    else
+        cout << "no";
+    cout << endl;
+
+    TraceX(P());
+    TraceX(Foo());
+    TraceX(FooD());
+}
