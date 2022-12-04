eca3c67520bab40845445fff7c0294363d2738f6
Vladimir Ivanov <knebekaizer@gmail.com>
2022-12-04 23:36:25 +0400

    wip: Implement map which keys may overflow / wraping

--- cpp-features/src/WrapOut.cpp
+++ cpp-features/src/WrapOut.cpp
@@ -2,6 +2,8 @@
 #include <algorithm>
 #include <type_traits>
 #include <map>
+#include <set>
+#include <vector>
 #include <ostream>
 
 #define STD_RANGES 0
@@ -30,7 +32,7 @@ namespace rg = ::ranges;
 #endif // __cpp_lib_ranges
 namespace vs = rg::views;
 
-#define TRACE_PRETTY
+//#define TRACE_PRETTY
 #include "trace.hpp"
 
 using namespace std;
@@ -170,40 +172,92 @@ ostream& operator<<(ostream& os, const S& s) {
 	return os << s.x;
 }
 
+
 using SP = unique_ptr<S>;
 class XMap {
 public:
 	using Map = std::multimap<T, SP>;
+	using value_type =  Map::value_type;
 	using It = Map::iterator;
 	auto equal_range(T x) const { return mp.equal_range(x); }
 	Map::const_iterator lower_bound(T x) const;
 
+	auto makeIt() {
+		return [this](){
+			It p = mp.begin();
+			if (sup < inf) {
+				p = mp.equal_range(inf).first;
+			}
+			return vs::iota(0u, mp.size()) | vs::transform([&](auto k) -> Map::value_type& {
+//			TraceX(k);
+				auto it = p;
+				TraceX(1, *it);
+				TraceX(2, (int)it->first, (int)it->second->x);
+				if (++p == mp.end()) p = mp.begin();
+				return *it;
+			});
+		};
+	}
+//	auto all() { return makeIt()(); }
+/*
 	auto all()  {
+		std::ostream& operator<<(std::ostream& os, Map::value_type const& v);
+//		return rg::make_subrange(mp.begin(), mp.end());
 		It p = mp.begin();
 		if (sup < inf) {
 			p = mp.equal_range(inf).first;
 		}
-		return vs::iota(0u, mp.size()) | vs::transform([p, this](auto k) mutable-> Map::value_type& {
-			auto it = p++;
-			if (p == mp.end()) p = mp.begin();
+//		TraceX(p != mp.end());
+//		TraceX(*p);
+//		return vs::iota(0u, mp.size()) | vs::transform(makeIt(p, this));
+		return vs::iota(0u, mp.size()) | vs::transform([&](auto k) -> Map::value_type& {
+//			TraceX(k);
+			auto it = p;
+//			TraceX(2, ret);
+			if (++p == mp.end()) p = mp.begin();
 			return *it;
 		});
 	}
+*/
 
-//	auto all2()  {
+	auto all()  {
+			return vs::iota(0, 2) | vs::transform([&](auto k) {
+				if (k == 0)
+					return rg::make_subrange(mp.find(inf), sup >= inf ? mp.upper_bound(sup) : mp.end());
+				else {
+					return rg::make_subrange(mp.begin(), sup >= inf ? mp.begin() : mp.upper_bound(sup));
+				}
+			})
+			| vs::join
+			| vs::take(mp.size())
+			;
+	}
+
+	auto all2()  {
 //		if (sup >= inf) {
 //			return rg::make_subrange(mp.begin(), mp.end());
 //		} else {
 //			auto [b, _] = mp.equal_range(inf);
+//			auto b = mp.find(inf);
 //			auto e = mp.upper_bound(sup);
-//			return vs::iota(0, 2) | vs::transform([&](auto k) {
-//				if (k == 0)
-//					return rg::make_subrange(b, mp.end());
-//				else
-//					return rg::make_subrange(mp.begin(), e);
-//			}) | vs::join;
+			return vs::iota(0, 2) | vs::transform([&](auto k) {
+				if (k == 0) {
+					return rg::make_subrange(mp.find(inf), mp.end());
+//					return rg::make_subrange(b, sup >= inf ? e : mp.end());
+				 } else {
+//					auto e = mp.upper_bound(sup);
+////					return rg::make_subrange(mp.begin(), sup >= inf ? mp.begin() : e);
+//					auto sbr = rg::make_subrange(mp.begin(), e);
+//					TraceX(rg::distance(sbr));
+//					return rg::make_subrange(mp.begin(), mp.end());
+					return rg::make_subrange(mp.begin(), mp.upper_bound(sup));
+				}
+			})
+//			| vs::take(1)
+			| vs::join | vs::take(mp.size());
 //		}
-//	}
+	}
+
 
 //	static bool less(T a, T b) { return isNewer(b, a); }
 	void push(T key, SP&& value) {
@@ -217,24 +271,58 @@ public:
 	}
 	void erase(It it) {
 		if (it == mp.end()) return;
-		if (mp.size() > 1 && inf != sup) {
-			if (it->first == inf) {
-				It p = it; ++p;
-				if (p == mp.end())
-					inf = mp.begin()->first;
-				else
-					inf = p->first;
+		if (mp.size() == 1 || inf == sup) {
+			// all keys are equal
+			mp.erase(it);
+			return;
+		}
+		// if we remove the first one (==inf) just advance `inf` to the next
+		if (it->first == inf) {
+			It p = it; ++p;
+			if (p == mp.end())
+				inf = mp.begin()->first; // wrap it aka overflow
+			else
+				inf = p->first;
+			mp.erase(it);
+		} else if (it->first == sup) {
+			// need to find new sup
+			// At FEC we only remove the oldest elements so this case should not occcure
+			if (inf <= sup) {
+				// trivial: no wrap
 				mp.erase(it);
-			} else if (it->first == sup) {
+				sup = mp.rbegin()->first;
+			} else  if (mp.begin()->first == sup) {
 				mp.erase(it);
-				It p = mp.lower_bound(sup);
-				if (p == mp.end()) {
-					p = mp.lower_bound(std::numeric_limits<T>::max());
-					assert(p != mp.end());
+				if (mp.begin()->first == sup) {
+					// there are more elements with the same key; keep sup intact
+				} else {
+					// wrap it and take the last key
+					sup = mp.rbegin()->first;
 				}
-				sup = p->first;
 			} else {
+				// There are lower keys so let's find the highest one < sup (binary search)
+				auto lo = mp.begin()->first;
+				auto hi = sup;
+				while (hi - lo > 1) {
+					auto mid = lo + (hi - lo)/2;
+					It up = mp.upper_bound(mid);
+					assert(up != mp.end());
+					if (up->first < sup) {
+						lo = mid;
+					} else {
+						hi = mid;
+					}
+				}
+				// hi may be better candidate; or hi may be = sup
 				mp.erase(it);
+				// check if key = sup still exists; otherwise set sup to prev
+				if (mp.find(sup) == mp.end()) {
+					// there is no such key anymore; move to previous
+					if (sup != hi)
+						sup = hi;
+					else
+						sup = lo;
+				}
 			}
 		} else {
 			mp.erase(it);
@@ -246,10 +334,8 @@ public:
 		auto b = mp.find(key);
 		while (b != mp.end()) {
 			erase(b);
-			TraceX(mp.size());
 			b = mp.find(key); // mp.equal_range(key).first;
 		}
-//		for (auto [b, e] = mp.equal_range(key); b != mp.end() && b != e; ++b) TraceX((uint)b->first); // erase(b);
 	}
 
 	std::multimap<T, SP> mp;
@@ -262,7 +348,7 @@ XMap::Map::const_iterator XMap::lower_bound(T x) const {
 }
 
 
-std::ostream& operator<<(std::ostream& os, XMap::Map::value_type const& v) {
+std::ostream& operator<<(std::ostream& os, XMap::value_type const& v) {
 	return os << '(' << (uint)v.first << " : " << (uint)v.second->x << ')';
 }
 
@@ -270,47 +356,101 @@ std::ostream& operator<<(std::ostream& os, std::pair<T, SP> const& v) {
 	return os << '(' << (uint)v.first << " : " << (uint)v.second->x << ')';
 }
 
+/// compare two ranges
+bool equals(auto&& a, auto&& b) {
+	auto pa = a.begin();
+	auto pb = b.begin();
+	for ( ; pa != a.end() && pb != b.end(); ++pa, ++pb) {
+//		*pa;
+//		TraceX(*pa, *pb, rg::distance(a));
+//		TraceX(*pa, *pb, rg::distance(a));
+//		TraceX(*pa, *pb, rg::distance(a));
+//		TraceX(*pa != *pb);
+////		TraceX(*pa != (decltype(*pa)) *pb);
+////		if (*pa != (decltype(*pa)) *pb) return false;
+		if (*pa != *pb) return false;
+	}
+//	TraceX(pa != a.end(), pb != b.end());
+	if (pa != a.end() || pb != b.end()) return false;
+	return true;
+//	return rg::distance(a) == rg::distance(b) &&
+//		rg::all_of(vs::zip(a, b), [](auto&& pair){ return pair.first == pair.second; });
+}
+
+void test_equals() {
+	assert( equals(vector<int>{1, 2, 3}, vector<int>{1, 2, 3}) );
+	assert( !equals(vector<int>{1, 2, 3}, vector<int>{1, 2, 4}) );
+	assert( !equals(vector<int>{1, 2, 3, 4}, vector<int>{1, 2, 3}) );
+
+	assert( equals(set{-1,0,1}, set{-1,0,1}) );
+	assert( !equals(set{-1,0,1}, set{-1,0,2}) );
+	assert( !equals(set{-1,0,1}, set{-1,0,1,2}) );
+
+	log_trace << "Test OK";
+}
+
+bool operator==(XMap::value_type const& x, XMap::value_type const& y) {
+	return x.first == y.first && x.second == y.second;
+}
+
+bool operator==(XMap::value_type const& x, pair<T, T> const& y) {
+	return x.first == y.first && x.second->x == y.second;
+}
+
+bool operator!=(XMap::value_type const& x, pair<T, T> const& y) {
+	return !(x == y);
+}
+
+bool equalsV(auto&& m, vector<pair<T, T>> const& v) {
+	return equals(m, v);
+}
+
 void test_XMap() {
+	test_equals();
+
 	XMap m;
 	auto push = [&](T k, T v = 0) { m.push(k, make_unique<S>(v)); };
+
 	for (T x : {1,2,3,4}) push(x);
 	push(3,1); push(3,2);
 	TraceX(m.mp);
 
-	TraceX((uint)m.inf, (uint)m.sup);
-	TraceX(m.all());
-	cout << '\n';
+	assert( equalsV(m.all(), {{1, 0}, {2, 0}, {3, 0}, {3, 1}, {3, 2}, {4, 0}}) );
+
+	cout << '\n'; TraceX((uint)m.inf, (uint)m.sup, m.all());
 
 	push(253); push(255); push(255, 255);
-	TraceX((uint)m.inf, (uint)m.sup);
-	TraceX(m.all());
-	cout << '\n';
+//	TraceX((uint)m.inf, (uint)m.sup);
+//	for (auto& x : m.all()) TraceX(x);
+//	TraceX(11);
+	assert( equalsV(m.all(), {{253, 0}, {255, 0}, {255, 255}, {1, 0}, {2, 0}, {3, 0}, {3, 1}, {3, 2}, {4, 0}}) );
+
+	TraceX((uint)m.inf, (uint)m.sup, m.all());
 
 	m.erase(1);
-	TraceX(1, (uint)m.inf, (uint)m.sup);
-	TraceX(m.all());
-	cout << '\n';
+	TraceX((uint)m.inf, (uint)m.sup, m.all());
+	assert( equalsV(m.all(), {{253, 0}, {255, 0}, {255, 255}, {2, 0}, {3, 0}, {3, 1}, {3, 2}, {4, 0}}) );
 
 	m.erase(42);
-	TraceX(42, (uint)m.inf, (uint)m.sup);
-	TraceX(m.all());
-	cout << '\n';
+	TraceX((uint)m.inf, (uint)m.sup, m.all());
+	assert( equalsV(m.all(), {{253, 0}, {255, 0}, {255, 255}, {2, 0}, {3, 0}, {3, 1}, {3, 2}, {4, 0}}) );
 
 	m.erase(253);
-	TraceX(253, (uint)m.inf, (uint)m.sup);
-	TraceX(m.all());
-	cout << '\n';
+	TraceX((uint)m.inf, (uint)m.sup, m.all());
+	assert( equalsV(m.all(), {{255, 0}, {255, 255}, {2, 0}, {3, 0}, {3, 1}, {3, 2}, {4, 0}}) );
 
 	m.erase(4);
-	TraceX(4, (uint)m.inf, (uint)m.sup);
-	TraceX(m.all());
-	cout << '\n';
+	TraceX((uint)m.inf, (uint)m.sup, m.all());
+	assert( equalsV(m.all(), {{255, 0}, {255, 255}, {2, 0}, {3, 0}, {3, 1}, {3, 2}}) );
 
 	m.erase(3);
-	TraceX(3, (uint)m.inf, (uint)m.sup);
-	TraceX(m.all());
-	cout << '\n';
+	TraceX((uint)m.inf, (uint)m.sup, m.all());
+	assert( equalsV(m.all(), {{255, 0}, {255, 255}, {2, 0}}) );
 
+	push(250);
+	m.erase(2);
+	TraceX((uint)m.inf, (uint)m.sup, m.all());
+	assert( equalsV(m.all(), {{250, 0}, {255, 0}, {255, 255}}) );
 }
 
 
