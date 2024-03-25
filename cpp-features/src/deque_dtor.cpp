6c619a1dc856e3f9e018e58893a93922d1c6f9b1
Vladimir Ivanov <knebekaizer@gmail.com>
2024-03-25 17:16:58 +0100

    Container allocator used for element and internal types

--- cpp-features/src/deque_dtor.cpp
+++ cpp-features/src/deque_dtor.cpp
@@ -2,15 +2,30 @@
 #include <array>
 #include <iostream>
 #include <deque>
-#include <typeinfo>
+#include <list>
 #include <set>
+#include <typeinfo>
 #include <functional>
 
-size_t num_constructed;
-size_t num_destroyed;
-size_t num_alloc;
-size_t num_dealloc;
+using namespace std;
+
 size_t num_dtor;
+
+static struct Stat {
+	size_t nConstruct = 0;
+	size_t nDestroy = 0;
+	size_t nAlloc = 0;
+	size_t nDealloc = 0;
+} stat;
+
+std::ostream& operator<<(ostream& os, Stat const& s) {
+	return os <<
+		"nAlloc: " << s.nAlloc << "; "
+		"nDealloc: " << s.nDealloc << "; "
+		"nConstruct: " << s.nConstruct << "; "
+		"nDestroy: " << s.nDestroy;
+}
+
 
 struct S {
 	S() { log_trace << "Default ctor " << typeid(S).name() << ": " << (void*)this; }
@@ -34,27 +49,27 @@ struct S {
 
 //template<class T = S>
 //using Tp = S;
-struct custom_allocator {
+struct alloc1 {
     using T = S;
     typedef T value_type;
 
-    custom_allocator() = default;
+    alloc1() = default;
 
 //    template<typename T_rhs>
-//    custom_allocator(const std::allocator<T_rhs>& rhs) { }
+//    alloc1(const std::allocator<T_rhs>& rhs) { }
 
     value_type * allocate(std::size_t n) {
         value_type * p = static_cast<value_type *>(::operator new(n * sizeof(value_type)));
-		++num_alloc;
-	    TraceX(typeid(T).name(), sizeof(T), this, p, num_alloc);
+		++stat.nAlloc;
+	    TraceX(typeid(T).name(), sizeof(T), this, p, stat.nAlloc);
 	    return p;
     }
 
     void deallocate(value_type * p, std::size_t n) {
         ::operator delete(p);
 //		delete (char*)p;
-	    ++num_dealloc;
-	    TraceX(typeid(T).name(), sizeof(T), this, p, num_dealloc);
+	    ++stat.nDealloc;
+	    TraceX(typeid(T).name(), sizeof(T), this, p, stat.nDealloc);
     }
 //    void construct(value_type * p, value_type value) {
 //        new ((void *) p) value_type(value);
@@ -64,38 +79,39 @@ struct custom_allocator {
 
     void construct(value_type * p) {
 //        new ((void *) p) value_type;
-        num_constructed++;
-	    TraceX(typeid(T).name(), sizeof(T), this, p, num_constructed);
+	    stat.nConstruct++;
+	    TraceX(typeid(T).name(), sizeof(T), this, p, stat.nConstruct);
     }
 
     void destroy(value_type * p) {
-        num_destroyed++;
+	    stat.nDestroy++;
         p->~value_type();
-	    TraceX(typeid(T).name(), sizeof(T), this, p, num_destroyed);
+	    TraceX(typeid(T).name(), sizeof(T), this, p, stat.nDestroy);
 	}
 	template <class U>
 	struct rebind { typedef std::allocator<U> other; };
+
+//	static struct Stat_ : Stat {} stat;
 };
 
 template<class T = S>
 struct alloc2 {
     typedef T value_type;
 
-//    template<typename T_rhs>
-//    custom_allocator(const std::allocator<T_rhs>& rhs) { }
+	alloc2() = default;
 
-    value_type * allocate(std::size_t n) {
-        value_type * p = static_cast<value_type *>(::operator new(n * sizeof(value_type)));
-		++num_alloc;
-	    TraceX(typeid(T).name(), sizeof(T), this, p, num_alloc);
+    value_type* allocate(std::size_t n) {
+        value_type* p = static_cast<value_type *>(::operator new(n * sizeof(value_type)));
+		++stat.nAlloc;
+	    TraceX(typeid(T).name(), sizeof(T), this, p, stat.nAlloc);
 	    return p;
     }
 
     void deallocate(value_type * p, std::size_t n) {
         ::operator delete(p);
 //		delete (char*)p;
-	    ++num_dealloc;
-	    TraceX(typeid(T).name(), sizeof(T), this, p, num_dealloc);
+	    ++stat.nDealloc;
+	    TraceX(typeid(T).name(), sizeof(T), this, p, stat.nDealloc);
     }
 //    void construct(value_type * p, value_type value) {
 //        new ((void *) p) value_type(value);
@@ -103,35 +119,89 @@ struct alloc2 {
 //	    TraceX(typeid(T).name(), sizeof(T), this, p, num_constructed);
 //    }
 
-    void construct(value_type * p) {
+//    void construct(value_type * p) {
 //        new ((void *) p) value_type;
-        num_constructed++;
-	    TraceX(typeid(T).name(), sizeof(T), this, p, num_constructed);
-    }
+//        num_constructed++;
+//	    TraceX(typeid(T).name(), sizeof(T), this, p, num_constructed);
+//    }
+
+	template <class _Up, class... _Args>
+	void construct(_Up* p, _Args&&... args) {
+		::new ((void*)p) _Up(std::forward<_Args>(args)...);
+		stat.nConstruct++;
+		TraceX(typeid(T).name(), sizeof(T), this, p, stat.nConstruct);
+	}
 
     void destroy(value_type * p) {
-        num_destroyed++;
+	    stat.nDestroy++;
         p->~value_type();
-	    TraceX(typeid(T).name(), sizeof(T), this, p, num_destroyed);
+	    TraceX(typeid(T).name(), sizeof(T), this, p, stat.nDestroy);
 	}
-	template <class U>
-	struct rebind { typedef std::allocator<U> other; };
+
+	template <class Up>
+    void destroy(Up* p) {
+	    stat.nDestroy++;
+        p->~Up();
+	    TraceX(typeid(T).name(), sizeof(T), this, p, stat.nDestroy);
+	}
+
+//	template <class U>
+//	struct rebind { typedef alloc2<U> other; };
+
+	template<typename U>
+	alloc2(const alloc2<U>& other) {
+		TraceF << "rebind allocator from the type " << typeid(U).name() << " to the type " << typeid(T).name();
+	}
+
 };
 
+
+template <class Alloc>
 void test_deque() {
-		std::deque<S, custom_allocator> d1(1);
+	cout << endl;
+	TraceF << typeid(Alloc).name();
+	stat = {0};
+	{
+		std::deque<S, Alloc> d1(1);
+	}
+	log_trace << stat;
 }
 
+
 void test_set() {
+	cout << endl;
 	TraceF;
+	stat = {0};
 	auto less = [](const S& s1, const S& s2) { return s1.c_ < s2.c_; };
 
-	std::set<S, decltype(less), alloc2<S>> d1;
-	d1.emplace();
-	d1.emplace();
+	using Alloc = alloc2<S>; // std::allocator<S>;
+	{
+		Alloc a;
+		std::set<S, decltype(less), Alloc> d1(less, a);
+		d1.emplace();
+		d1.emplace();
+	}
+	log_trace << stat;
+}
+
+template <class Alloc>
+void test_list() {
+	cout << endl;
+	TraceF << typeid(Alloc).name();
+	stat = {0};
+	{
+		std::list<S, Alloc> d1;
+		d1.emplace_back();
+		d1.emplace_back();
+	}
+	log_trace << stat;
 }
 
 int main() {
-	test_deque();
+//	test_deque<alloc1>();
+	test_deque<alloc2<S>>();
 	test_set();
+
+	test_list<alloc1>();
+	test_list<alloc2<S>>();
 }
