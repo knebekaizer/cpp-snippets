5e7a1de6807ee7f99d56b80bd9d5e782a8c22b52
Vladimir Ivanov <vladimir.ivanov@validas.de>
2024-04-02 21:14:58 +0200

    wip

--- cpp-features/src/float_random.cpp
+++ cpp-features/src/float_random.cpp
@@ -2,22 +2,34 @@
 #include <random>
 using namespace std;
 
-
 template <typename RE>
 int testSingle(size_t seed) {
 	std::uniform_real_distribution<float> distribution(0.0f, 2.0f);
 	auto g = RE(seed);
 	const size_t N = 2000000;
+    cout << boolalpha;
 	for (size_t i = 0; i < N; ++i) {
 		auto x = distribution(g);
-		if (x >= distribution.max()) {
-			cout << "FAILED! seed: " << seed <<
-					"; iterations: " << i <<
-					"; value: " << x <<
-					"; max: " << distribution.max() << endl;
+		if (x >= distribution.b()) {
+			cout << "FAILED! seed: " << seed << "; iterations: " << i << "; value: " << x
+                 << "; max: " << distribution.b()
+                 << "; (b-x): " << (distribution.b() - x)
+                 << "; (x-a): " << (x - distribution.a())
+                 << "; int(x-a): " << (int)(x - distribution.a())
+                 << endl;
 			return -1;
 		}
-	}
+        if ((is_same<RE, std::mt19937>::value && seed == 3 && i == 1414715)
+            || (is_same<RE, std::minstd_rand>::value && seed == 25 && i == 689064)) {
+            cout << "PASSED! seed: " << seed << "; iterations: " << i << "; value: " << x
+                 << "; max: " << distribution.b()
+                 << "; x < b: " << (x < distribution.b())
+                 << "; b-x = " << (distribution.b() - x)
+                 << "; x-a = " << (x - distribution.a())
+                 << "; int(x-a) = " << (int)(x - distribution.a())
+                 << endl;
+        }
+    }
 	return 0;
 }
 
@@ -33,10 +45,10 @@ int testRandom() {
 #define testRandom(engine) {cout << #engine << ": "; testRandom<engine>(); }
 
 int testAllRandom() {
-	testRandom(std::knuth_b);
+    testRandom(std::knuth_b);
     testRandom(std::minstd_rand0);
-	testRandom(std::minstd_rand);
-	testRandom(std::mt19937);
+    testRandom(std::minstd_rand);
+    testRandom(std::mt19937);
     testRandom(std::mt19937_64);
     testRandom(std::ranlux24);
     testRandom(std::ranlux24_base);
@@ -44,7 +56,6 @@ int testAllRandom() {
     testRandom(std::ranlux48_base);
     return 0;
 }
-
 
 int main() {
     testAllRandom();
