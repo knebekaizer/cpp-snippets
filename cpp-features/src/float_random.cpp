37a437afa782486d636f0524956d05b7103ff0ad
Vladimir Ivanov <knebekaizer@gmail.com>
2024-04-02 19:50:41 +0200

    Minimal float random test
    std::knuth_b: FAILED! seed: 10; iterations: 1718564; value: 2; max: 2
    std::minstd_rand0: FAILED! seed: 10; iterations: 1718508; value: 2; max: 2
    std::minstd_rand: FAILED! seed: 25; iterations: 689064; value: 2; max: 2
    std::mt19937: FAILED! seed: 3; iterations: 1414715; value: 2; max: 2
    std::mt19937_64: FAILED! seed: 1; iterations: 517518; value: 2; max: 2
    std::ranlux24: PASSED!
    std::ranlux24_base: PASSED!
    std::ranlux48: FAILED! seed: 9; iterations: 1548908; value: 2; max: 2
    std::ranlux48_base: FAILED! seed: 25; iterations: 1759623; value: 2; max: 2

--- cpp-features/src/float_random.cpp
+++ cpp-features/src/float_random.cpp
@@ -1,52 +1,20 @@
-//
-// Created by vdi on 28.03.24.
-//
-
-#include "trace.hpp"
-//#include <cassert>
-#include <stdio.h>
+#include <iostream>
 #include <random>
 using namespace std;
 
-//template<class G>
-//typename std::enable_if<std::is_same<G, std::ranlux24>::value ||
-//                            std::is_same<G, std::ranlux48>::value ||
-//                            std::is_same<G, std::knuth_b>::value,
-//                        G>::type
-//    create_generator() {
-//    TraceX(1, typeid(G).name());
-//    std::mt19937 gen(42);
-//    return G(gen());
-//}
-//template<class G>
-//typename std::enable_if<!std::is_same<G, std::ranlux24>::value &&
-//                            !std::is_same<G, std::ranlux48>::value &&
-//                            !std::is_same<G, std::knuth_b>::value,
-//                        G>::type
-//    create_generator() {
-//    TraceX(2, typeid(G).name());
-//    return G(std::random_device{}());
-//}
-
-
-struct Stat {
-	size_t seed = 0;
-	size_t iter = numeric_limits<decltype(iter)>::max();
-} stat;
 
 template <typename RE>
 int testSingle(size_t seed) {
-	using F = float;
-	std::uniform_real_distribution<F> distribution(0.0f, 2.0f);
-//    auto g = RE(random_device{}());
-//    auto g = RE{std::random_device{}()}; //create_generator<RE>();
+	std::uniform_real_distribution<float> distribution(0.0f, 2.0f);
 	auto g = RE(seed);
 	const size_t N = 2000000;
 	for (size_t i = 0; i < N; ++i) {
 		auto x = distribution(g);
 		if (x >= distribution.max()) {
-			printf("FAILED! seed: %lu, iterations: %lu; value: %f, max: %f\n", seed, i, x, distribution.max());
-			if (i < stat.iter) stat = {seed, i};
+			cout << "FAILED! seed: " << seed <<
+					"; iterations: " << i <<
+					"; value: " << x <<
+					"; max: " << distribution.max() << endl;
 			return -1;
 		}
 	}
@@ -54,87 +22,31 @@ int testSingle(size_t seed) {
 }
 
 template <typename RE>
-int testRandom(const char* name) {
-	cout << name << ": ";
-	for (size_t seed = 0; seed < 100; ++seed) {
-		if (testSingle<RE>(seed)) return 1;
-	}
-	return 0;
-}
-
-template <typename RE>
 int testRandom() {
-	cout << typeid(RE).name() << ": ";
 	for (size_t seed = 0; seed < 100; ++seed) {
 		if (testSingle<RE>(seed)) return 1;
 	}
+	cout << "PASSED!\n";
 	return 0;
 }
 
-//#define testRandom(engine) testRandom<engine>(#engine)
+#define testRandom(engine) {cout << #engine << ": "; testRandom<engine>(); }
 
 int testAllRandom() {
-//    testRandom<std::default_random_engine>();
-//	testRandom(knuth_b);
-//    testRandom<std::minstd_rand0>();
-	testRandom<std::minstd_rand>();
-	testRandom<std::mt19937>();
-    testRandom<std::mt19937_64>();
-    testRandom<std::ranlux24>();
-//    testRandom<std::ranlux24_base>();
-//    testRandom<std::ranlux48>();
-//    testRandom<std::ranlux48_base>();
+	testRandom(std::knuth_b);
+    testRandom(std::minstd_rand0);
+	testRandom(std::minstd_rand);
+	testRandom(std::mt19937);
+    testRandom(std::mt19937_64);
+    testRandom(std::ranlux24);
+    testRandom(std::ranlux24_base);
+    testRandom(std::ranlux48);
+    testRandom(std::ranlux48_base);
     return 0;
 }
 
+
 int main() {
     testAllRandom();
-return 0;
-	log_trace << "minstd_rand";
-    for (size_t seed = 0; seed < 100; ++seed) {
-        testSingle<minstd_rand>(seed);
-    }
-	TraceX(stat.seed, stat.iter);
-
-	log_trace << "mt19937";
-	stat = Stat{};
-	for (size_t seed = 0; seed < 100; ++seed) {
-//        if (testSingle<mt19937>(seed)) break;
-        testSingle<mt19937>(seed);
-    }
-	TraceX(stat.seed, stat.iter);
-
-	{
-		log_trace << "mt19937";
-		stat = Stat{};
-		random_device e;
-		for (size_t k = 0; k < 100; ++k) {
-			auto seed = e();
-			testSingle<mt19937>(seed);
-		}
-		TraceX(stat.seed, stat.iter);
-	}
-	log_trace << "mt19937";
-	stat = Stat{};
-	default_random_engine e;
-    for (size_t k = 0; k < 100; ++k) {
-		auto seed = e();
-        testSingle<mt19937>(seed);
-    }
-	TraceX(stat.seed, stat.iter);
-	return 0;
-
-	log_trace << "knuth_b";
-	stat = Stat{};
-	for (size_t seed = 0; seed < 100; ++seed) {
-		testSingle<knuth_b>(seed);
-	}
-	TraceX(stat.seed, stat.iter);
-	log_trace << "ranlux48";
-	stat = Stat{};
-	for (size_t seed = 0; seed < 100; ++seed) {
-		testSingle<ranlux48>(seed);
-	}
-	TraceX(stat.seed, stat.iter);
 	return 0;
 }
