2787eb205ebe10826d342823bdf340660c2d073f
Vladimir Ivanov <knebekaizer@gmail.com>
2025-02-12 19:14:11 +0100

    [literals] Better fix fo c++14 using compile-time hash

--- cpp-features/src/literals.cpp
+++ cpp-features/src/literals.cpp
@@ -37,29 +37,49 @@ template <typename T, T... index> constexpr NoNum s2e(const char* str, std::inte
 	return static_cast<NoNum>(found);
 }
 #else
-constexpr int s2e_unfold(const char* str) {
+template <typename Str>
+constexpr size_t hashString(const Str& toHash)
+{
+	static_assert(sizeof(size_t) == 8);
+	// FNV-1a 64 bit algorithm
+	size_t result = 0xcbf29ce484222325; // FNV offset basis
+	for (auto const* p = toHash; *p; ++p  ) { const char c = *p;
+//	for (char c : toHash) {
+		result ^= c;
+		result *= 1099511628211; // FNV prime
+	}
+	return result;
+}
+
+constexpr int s2e_unfold(const char str[]) {
 	return -1;
 }
 
 template <typename T, class...Ts>
 constexpr T s2e_unfold(const char* str, T&& i, Ts&&...as) {
-	if (str == NumS[i]) return i;
+//template <typename Str, typename T, class...Ts>
+//constexpr T s2e_unfold(const Str& str, T&& i, Ts&&...as) {
+//	if (str == NumS[i]) return i;
+	if (hashString(str) == hashString(NumS[i])) return i;
+//	if (hashString(str) == 0) return i;
+//	if (str == nullptr) return i;
+//	if (std::get<0>(NumS) == NumS[i]) return i; // ok
 	return s2e_unfold(str, std::forward<Ts>(as)...);
 }
 
-template <typename T, T... index>
-constexpr NoNum s2e(const char* str, std::integer_sequence<T, index...> seq) {
+template <typename Str, typename T, T... index>
+constexpr NoNum s2e(const Str& str, std::integer_sequence<T, index...> seq) {
 	return static_cast<NoNum>(s2e_unfold(str, index...));
 }
 
 #endif
 
-
-constexpr NoNum s2e(const char* str) {
+template <typename Str>
+constexpr NoNum s2e(const Str& str) {
     return s2e(str, std::make_index_sequence<NumS.size()>{});
 }
 
-constexpr enum NoNum operator""_f (const char* str, std::size_t) {
+constexpr enum NoNum operator""_f (const char str[], std::size_t) {
     return s2e(str);
 }
 
@@ -73,7 +93,22 @@ void bar() {
     TraceX(n);
 }
 
+template <size_t n>
+void foo() {
+    TraceX(n);
+}
 
+// check the literal type:
+template <typename Str>
+void checkLiteralType(Str&& str) {
+	log_trace << "checkLit<Str>(Str&& str) template function: " << typeid(Str).name();
+	for (char c : str) { cout << c; } cout << endl;
+}
+
+//template <typename Char>
+//void checkLiteralType(const Char str[]) {
+//	log_trace << "checkLit(const char str[]) plain function: " << str;
+//}
 
 int main() {
 //	test();
@@ -92,8 +127,35 @@ int main() {
 //	TraceX((string{'a', 'b', 'c'}));
 
     bar<static_cast<NoNum>(21)>();
+    bar<s2e("one")>();
     bar<"one"_f>();
     bar<"two"_f>();
     bar<"blabla"_f>();
+	foo<hashString("one")>();
+	checkLiteralType("one");
+	TraceX(typeid(char[4]).name());
+	TraceX(typeid(const char[4]).name());
+	const char one[] = "one";
+	TraceX(typeid(const char*).name());
+	checkLiteralType(one);
 	return 0;
 }
+
+/*
+template <int N>
+struct string_litteral {
+	constexpr string_litteral(const char (&str)[N]) {
+		std::copy_n(str, N, value);
+	}
+	char value[N+1]{};
+};
+
+template <string_litteral x>
+void foo() {
+	TraceX(x.value);
+}
+
+void test() {
+	foo<"qwerty">();
+}
+*/
